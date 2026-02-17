a732a035b4102b3eb5a8454292eab06157e0f84b
anshulthakkar5 <204286406+anshulthakkar5@users.noreply.github.com>
2026-02-17 11:12:30 -0600

    Update App.java

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -2,7 +2,18 @@ package org.example;
 
 public class App {
   public static void main(String[] args) {
-    System.out.println("Write a driver program for the Converter class here!");
-    System.out.println("You can delete these print statements and replace them with your code.");
+    Converter c = new Converter();
+
+    System.out.println("25C -> F = " + c.celsiusToFahrenheit(25));
+    System.out.println("77F -> C = " + c.fahrenheitToCelsius(77));
+
+    System.out.println("10 in -> cm = " + c.inchesToCentimeters(10));
+    System.out.println("25.4 cm -> in = " + c.centimetersToInches(25.4));
+
+    System.out.println("5 miles -> km = " + c.milesToKilometers(5));
+    System.out.println("8.04672 km -> miles = " + c.kilometersToMiles(8.04672));
+
+    System.out.println("150 lb -> kg = " + c.poundsToKilograms(150));
+    System.out.println("68.0389 kg -> lb = " + c.kilogramsToPounds(68.0389));
   }
 }
