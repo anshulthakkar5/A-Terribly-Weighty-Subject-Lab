f49cbd9f68b6b8c67737eaf40cd139241f83fd2a
anshulthakkar5 <204286406+anshulthakkar5@users.noreply.github.com>
2026-02-17 11:12:02 -0600

    Update Converter.java

--- app/src/main/java/org/example/Converter.java
+++ app/src/main/java/org/example/Converter.java
@@ -1,14 +1,42 @@
 package org.example;
 
 public class Converter {
-  // Write the code needed to make the tests in
-  // TestConverter.java pass. I have provided the
-  // tests for you in this lab to give you an example
-  // of how they should look. In the future, you will
-  // be responsible for writing your own tests.
 
-  // After you have written the code to make the tests
-  // pass, exercise your code in a driver function in
-  // App.java. This shows that your business logic can
-  // run in a console program.
-}
+  // Temperature
+  public double celsiusToFahrenheit(double c) {
+    return (c * 9.0 / 5.0) + 32.0;
+  }
+
+  public double fahrenheitToCelsius(double f) {
+    return (f - 32.0) * 5.0 / 9.0;
+  }
+
+  // Length
+  public double inchesToCentimeters(double in) {
+    return in * 2.54;
+  }
+
+  public double centimetersToInches(double cm) {
+    return cm / 2.54;
+  }
+
+  public double milesToKilometers(double miles) {
+    return miles * 1.609344;
+  }
+
+  public double kilometersToMiles(double km) {
+    return km / 1.609344;
+  }
+
+  // Mass
+  public double poundsToKilograms(double lb) {
+    return lb * 0.45359237;
+  }
+
+  public double kilogramsToPounds(double kg) {
+    return kg / 0.45359237;
+  }
+
+  // Optional helper if tests expect rounding
+  public double roundTo(double value, int decimals) {
+    if (decimals < 0) throw new IllegalArgument
